e0fe054b4269a82be4dcbae4c1c3d533062003d6
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-03 16:54:25 -0700

    initial commit with first test using assert

---
+++ test/first_spec.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+// build in node assert library
+
+describe("A feature", function(){
+  describe("A scenario", function(){
+    it("does something expected", function() {
+      assert(true);
+    });
+  });
+});
